92360bd1ad8282d32b670948a79d5c4d00ed5767
HabiburRahmanHilal <146487473+HabiburRahmanHilal@users.noreply.github.com>
2024-01-10 20:24:15 +0430

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 6000;
+
+export const mongoDBURL =  'mongodb+srv://habib:habib123456@cluster1.gycgtxj.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
